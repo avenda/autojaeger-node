2e12cf6d5101987febb635376f589cb91cca8269
Victor E Avendaño <veavendano@falabella.cl>
2018-12-10 15:14:07 -0300

    fix travis indentation

--- src/autojaeger.js
+++ src/autojaeger.js
@@ -1,10 +1,22 @@
 import jaeger from 'jaeger-client'
+import reporters from 'jaeger-client/dist/src/reporters/'
 
 class AutoJaeger {
+
+
   constructor({
-    serviceName,
-    sampler
+    serviceName, 
+    sampler = new jaeger.RateLimitingSampler(1),
+    remote 
   }) {
+    if (!serviceName) { throw new Error("serviceName is required!") }
+
+    if (!sampler) { throw new Error("sampler id required!") }
+
+    this._tracer = new jaeger.Tracer(
+      serviceName,
+      
+    )
 
   }
 }
