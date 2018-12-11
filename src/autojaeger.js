50d48440ecd5975d3c344962c5878ef9e1ed3e3d
Victor E Avendaño <veavendano@falabella.cl>
2018-12-11 16:05:28 -0300

    jaeger import simple bootstraping

--- src/autojaeger.js
+++ src/autojaeger.js
@@ -1,8 +1,25 @@
+import Instrument from '@risingstack/opentracing-auto';
+import jaeger from 'jaeger-client';
+import UDPSender from 'jaeger-client/dist/src/reporters/udp_sender';
+
 class AutoJaeger {
   constructor(options) {
     if (!options.serviceName) { throw new Error('serviceName is required!'); }
-
     if (!options.sampler) { throw new Error('sampler id required!'); }
+    const sender = new UDPSender(
+      {
+        host: 'localhost',
+        port: 123,
+      },
+    );
+    const tracer = new jaeger.RemoteReporter(sender);
+    this.instrument = new Instrument(
+      {
+        tracers: [
+          tracer,
+        ],
+      },
+    );
   }
 }
 
--- test/unit/autojaeger.test.js
+++ test/unit/autojaeger.test.js
@@ -4,6 +4,14 @@ import AutoJaeger from '../../src/autojaeger';
 
 describe('AutoJaeger Tests', () => {
   it('should load constructor', () => {
-    chai.expect(new AutoJaeger({ serviceName: 'dummy-service-name' })).to.be.an.instanceof(AutoJaeger);
+    chai.expect(
+      new AutoJaeger(
+        {
+          serviceName: 'dummy-service-name',
+          sampler: 'dummy-sampler-id',
+        },
+      ),
+    )
+      .to.be.an.instanceof(AutoJaeger);
   });
 });
