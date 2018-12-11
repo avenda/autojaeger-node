a91229094d738bb3a34fe9e42e997a739e4f037d
Victor E Avendaño <veavendano@falabella.cl>
2018-12-11 12:12:58 -0300

    * linter config update

--- test/unit/autojaeger.test.js
+++ test/unit/autojaeger.test.js
@@ -1,10 +1,9 @@
-import chai from 'chai'
-import {AutoJaeger} from '../../src/autojaeger'
+import chai from 'chai';
+import AutoJaeger from '../../src/autojaeger';
 
-const expect = chai.expect;
 
 describe('AutoJaeger Tests', () => {
   it('should load constructor', () => {
-    expect(new AutoJaeger({serviceName: "dummy-service-name", })).to.be.an.instanceof(AutoJaeger);
-  })
-})
+    chai.expect(new AutoJaeger({ serviceName: 'dummy-service-name' })).to.be.an.instanceof(AutoJaeger);
+  });
+});
